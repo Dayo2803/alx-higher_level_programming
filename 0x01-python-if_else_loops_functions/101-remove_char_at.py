d168be6eddbbf76945ec0e0fe887713971d93896
Dayo <folushooyinkansola@gmail.com>
2023-11-28 13:03:54 -0800

    remove

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,5 +3,5 @@ def remove_char_at(str, n):
     string_cpy = ''
     for i in range(len(str)):
         if i != n:
-            string_copy += str[i]
-    return (string_copy)
+            string_cpy += str[i]
+    return (string_cpy)
