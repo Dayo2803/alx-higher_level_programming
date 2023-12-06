df5573276cde51a5b81f389c94c5b546d75869d9
Dayo <folushooyinkansola@gmail.com>
2023-12-06 05:26:14 -0800

    square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = acpy_matrix.copy()
-    for i in range(0, len(acpy_matrix)):
-        new_matrix[i] = list(map(lambda x: x**2, acpy_matrix[i]))
+    new_matrix = matrix.copy()
+    for i in range(0, len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
     return new_matrix
