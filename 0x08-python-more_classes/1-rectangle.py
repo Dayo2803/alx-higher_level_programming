73cb5d1816942a929fed64542dfc4639ab177611
Dayo2803 <139994374+Dayo2803@users.noreply.github.com>
2024-01-04 13:47:37 +0100

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
-""" empty class Rectangle that defines a rectangle
+"""
+empty class Rectangle that defines a rectangle
 """
 
 
 class Rectangle:
-    """ class rectangle"""
+    """class rectangle"""
     def __init__(self, width=0, height=0):
-        """ Instantiation with optional width and height"""
+        """Instantiation with optional width and height"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """ width
-        """
+        """width"""
         return self.__width
 
     @property
     def height(self):
-        """height
-        """
+        """height"""
         return self.__height
 
     @width.setter
     def width(self, value):
-        """ width setter
-        """
+        """width setter"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -34,8 +32,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """ height setter
-        """
+        """height setter"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
